e2e791d5d51d9d550d60e2c9c732e4183093d708
Andrii Holovan <vozhd1908@gmail.com>
2024-06-05 14:48:00 +0200

    [feat] add temporary WIP banner to Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Button from '@/app/_components/button';
 import HandIcon from '@/app/_components/hand-icon';
 import Heading from '@/app/_components/heading';
@@ -5,21 +6,38 @@ import { getQuizList } from '@/app/_lib/get-quiz-data';
 
 import styles from '@/app/page.module.css';
 
+function TempBanner() {
+  return (
+    <div className={styles.banner}>
+      work in progress. stay tuned.
+      <Image
+        src="https://a.storyblok.com/f/181188/x/4a171ac405/logo-short-light-mode.svg"
+        alt="Headway logo"
+        width={16}
+        height={16}
+      />
+    </div>
+  );
+}
+
 export default async function Home() {
   const [firstQuizOption] = await getQuizList();
   return (
-    <main className={styles.main}>
-      <div className={styles.logo_container}>
-        <HandIcon />
-      </div>
-      <div className={styles.content_container}>
-        <Heading as="h1" className={styles.heading}>
-          Who wants to be a&nbsp;millionaire?
-        </Heading>
-        <Button push to={`/quiz/${firstQuizOption.step}`}>
-          Start
-        </Button>
-      </div>
-    </main>
+    <>
+      <TempBanner />
+      <main className={styles.main}>
+        <div className={styles.logo_container}>
+          <HandIcon />
+        </div>
+        <div className={styles.content_container}>
+          <Heading as="h1" className={styles.heading}>
+            Who wants to be a&nbsp;millionaire?
+          </Heading>
+          <Button push to={`/quiz/${firstQuizOption.step}`}>
+            Start
+          </Button>
+        </div>
+      </main>
+    </>
   );
 }
