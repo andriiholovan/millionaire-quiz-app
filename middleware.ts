c47023742fa42036585b4a303581b304c835c859
Andrii Holovan <vozhd1908@gmail.com>
2024-06-11 17:17:41 +0200

    [feat] add middleware to guard next quiz questions

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { STEP } from '@/app/_lib/cookies';
+
+export function middleware(request: NextRequest) {
+  const stepFromCookie = request.cookies.get(STEP)?.value;
+  const stepFromUrl = request.url.split('/').at(-1);
+
+  if (stepFromUrl === '1') {
+    return NextResponse.next();
+  }
+  if (!stepFromCookie) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+  if (stepFromUrl !== stepFromCookie) {
+    return NextResponse.redirect(new URL(`/quiz/${stepFromCookie}`, request.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: '/quiz/:path*',
+};
