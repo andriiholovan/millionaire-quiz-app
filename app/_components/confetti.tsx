5a7732410e847ae28f26ea70f57011b96f83a745
Andrii Holovan <vozhd1908@gmail.com>
2024-07-03 18:22:21 +0200

    [feat] update fireworks animation

--- app/_components/confetti.tsx
+++ app/_components/confetti.tsx
@@ -1,22 +1,37 @@
-import styles from '@/app/_components/confetti.module.css';
+'use client';
 
-const MAX_24_BIT_COLOR = 16777215;
+import Fireworks from 'react-canvas-confetti/dist/presets/fireworks';
 
-export default function Confetti() {
+function Confetti() {
   return (
-    <div className={styles.confetti}>
-      {[...Array(400)].map(() => (
-        <div
-          // Math.random() is ok for key prop, as far as the context is Server Component
-          key={Math.random()}
-          className={styles.ball}
-          style={{
-            background: `#${Math.floor(Math.random() * MAX_24_BIT_COLOR).toString(16)}`,
-            animationDuration: `${Math.random() * 9 + 2}s`,
-            animationDelay: `${Math.random() * 2}s`,
-          }}
-        />
-      ))}
-    </div>
+    <>
+      <Fireworks
+        autorun={{
+          speed: 1,
+          duration: 1000,
+        }}
+        decorateOptions={() => ({
+          origin: { x: 0, y: 1 },
+          angle: 70,
+          particleCount: 200,
+          startVelocity: 120,
+        })}
+      />
+      <Fireworks
+        autorun={{
+          speed: 1,
+          duration: 1000,
+          delay: 500,
+        }}
+        decorateOptions={() => ({
+          origin: { x: 1, y: 1 },
+          angle: 110,
+          particleCount: 200,
+          startVelocity: 120,
+        })}
+      />
+    </>
   );
 }
+
+export default Confetti;
