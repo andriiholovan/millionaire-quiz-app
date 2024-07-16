bf1a2679df265682ba9c678c0be2d0180bb955c0
Andrii Holovan <vozhd1908@gmail.com>
2024-07-16 21:04:08 +0200

    [feat] add vercel toolbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { VercelToolbar } from '@vercel/toolbar/next';
 import '@/app/_styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -14,9 +15,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const shouldInjectToolbar: boolean = process.env.NODE_ENV === 'development';
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        {shouldInjectToolbar && <VercelToolbar />}
+      </body>
     </html>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
+import withVercelToolbar from '@vercel/toolbar/plugins/next';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+export default withVercelToolbar()(nextConfig);
