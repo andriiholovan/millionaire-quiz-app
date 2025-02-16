ff474e0ad79657d741c0a8ff737a7f7d8e246741
Andrii Holovan <vozhd1908@gmail.com>
2025-02-16 17:30:23 +0100

    [chore] migrate to next@15

--- app/_lib/cookies.ts
+++ app/_lib/cookies.ts
@@ -3,8 +3,9 @@ import { cookies } from 'next/headers';
 const ONE_HOUR = 3_600_000; // one-hour period
 export const STEP = 'millionaire-quiz-step';
 
-export function setCookie(key: string, value: number): void {
-  cookies()
+export async function setCookie(key: string, value: number): Promise<void> {
+  const cookieStore = await cookies();
+  cookieStore
     .set(
       key,
       JSON.stringify(value),
@@ -15,6 +16,7 @@ export function setCookie(key: string, value: number): void {
     );
 }
 
-export function deleteCookie(key: string): void {
-  cookies().delete(key);
+export async function deleteCookie(key: string): Promise<void> {
+  const cookieStore = await cookies();
+  cookieStore.delete(key);
 }
